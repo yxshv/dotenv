5515d8e2ab9b689b753c5fcc6622056ee7d91648
kekda-py <yashverma30115@gmail.com>
2022-05-21 22:16:56 +0530

    add remove command

---
+++ cmd/remove.go
@@ -0,0 +1,79 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var removeCmd = &cobra.Command{
+	Use:   "remove",
+	Short: "Removes a variable from the file",
+	Long:  "Removes a variable from the file",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if len(args) < 1 {
+			fmt.Println("Your are missing the <NAME> argument.")
+			return
+		}
+
+		givenName := args[0]
+
+		fileFlag, err := cmd.Flags().GetString("file")
+
+		check(err)
+
+		if fileFlag == "" {
+			fileFlag = ".env"
+		}
+
+		if strings.HasPrefix(fileFlag, ".env") == false {
+			fmt.Println("The file needs to startwith `.env`")
+			return
+		}
+
+		fmt.Printf("Checking for `%s`\n", fileFlag)
+
+		content, err := ioutil.ReadFile(fileFlag)
+
+		if err != nil {
+			if strings.HasSuffix(err.Error(), "no such file or directory") == true {
+				fmt.Println("file not found. so creating one..")
+
+				os.Create(fileFlag)
+
+				content, err = ioutil.ReadFile(fileFlag)
+
+				check(err)
+
+				fmt.Println("successfully created")
+			}
+		}
+
+		variables := make(map[string]string)
+
+		for _, v := range strings.Split(string(content), "\n") {
+			if len(v) < 0 || v == "" {
+				continue
+			}
+			name, value := parseVar(v, "=")
+			variables[name] = value
+		}
+
+		delete(variables, givenName)
+
+		content_bytes := []byte(stringify(variables))
+		os.WriteFile(fileFlag, content_bytes, 0644)
+		check(err)
+
+		fmt.Printf("\nSuccessfully removed the variable `%s` (if it existed) from `%s`\n", givenName, fileFlag)
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(removeCmd)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -37,20 +37,8 @@ var slashT string = "   "
 
 var rootCmd = &cobra.Command{
 	Use:   "dotenv",
-	Short: "Dotenv allows u to manage your .env files",
-	Long:  "Dotenv allows u to manage your .env files",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("\n%sWelcome to Dotenv\n\n", slashT)
-
-		fmt.Printf("%sCommands:\n\n", slashT)
-		fmt.Printf("%s%slist - List all the variables in the .env file\n", slashT, slashT)
-		fmt.Printf("%s%schange <NAME> <VALUE> - Changes the variable if it exists else creates it\n", slashT, slashT)
-		fmt.Printf("%s%sremove <NAME> - Removes a variable from the file", slashT, slashT)
-		fmt.Println("\n ")
-
-		fmt.Printf("%sFlags:\n", slashT)
-		fmt.Printf("\n%s%sfile - The file to read from\n\n", slashT, slashT)
-	},
+	Short: "Dotenv allows you to manage your .env files",
+	Long:  "Dotenv allows you to manage your .env files",
 }
 
 func init() {
